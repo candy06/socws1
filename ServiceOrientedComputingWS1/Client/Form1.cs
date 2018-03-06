5e858228ccfef5d458e2d7285d952c628418bfa8
Loic rose <loic.rose@etu.unice.fr>
2018-03-06 15:59:02 +0100

    Implémentation du client - possibilité de cliquer sur un bouton pour afficher la liste des villes (interface graphique).

---
+++ ServiceOrientedComputingWS1/Client/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using Client.VelibSOAPService;
+
+namespace Client
+{
+    public partial class Form1 : Form
+    {
+
+        private ServiceClient client = new ServiceClient();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            City[] cities = client.GetCities();
+            for (int i = 0; i < cities.Length; i++)
+            {
+                citiesList.Items.Add(cities[i].name);
+            }
+        }
+    }
+}
