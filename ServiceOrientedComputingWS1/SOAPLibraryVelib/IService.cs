3c96df01d5bbcf2ed658dbd1ddefd3a94293c04e
Loic ROSE <loic.rose@etu.unice.fr>
2018-03-15 11:36:42 +0100

    Ajout d'un cache pour l'IWS et utilisation dans la methode GetCities.

--- ServiceOrientedComputingWS1/SOAPLibraryVelib/IService.cs
+++ ServiceOrientedComputingWS1/SOAPLibraryVelib/IService.cs
@@ -1,7 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Runtime.Serialization;
 using System.ServiceModel;
-using System.Threading.Tasks;
 
 namespace SOAPLibraryVelib
 {
